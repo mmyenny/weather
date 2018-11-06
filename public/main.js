97b5ada9d5b6f0dda4d3fa02387618a77ff82204
Michelle Yenny <mmy491@gmail.com>
2018-11-06 18:05:22 -0500

    Done with explorer mode

--- public/main.js
+++ public/main.js
@@ -1,40 +1,112 @@
-const addWeatherToPage = weather => {
-  let weatherInformation = document.querySelector('ul')
-  // zap all the html inside that UL (delete whatever might already be there)
-  weatherInformation.innerHTML = ''
+class WeatherAPI {
+  getWeatherByCity(cityName) {
+    // do all the work to fetch the data
+    let cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=c41f56ca4bf0ad336c93e8389a094c98`
 
-  let tempLI = document.createElement('li')
-  tempLI.textContent = `Temperature: ${weather.main.temp} F`
-  weatherInformation.appendChild(tempLI)
+    fetch(cityURL)
+      .then(response => {
+        return response.json()
+      })
+      .then(weatherJson => {
+        let weather = new Weather(weatherJson)
 
-  let humidityLI = document.createElement('li')
-  humidityLI.textContent = `Humidity: ${weather.main.humidity}%`
-  weatherInformation.appendChild(humidityLI)
+        let dom = new DocumentObjectModel()
+        dom.addWeatherToPage(weather.temp(), weather.humidity(), weather.wind())
+      })
+  }
 
-  let windLI = document.createElement('li')
-  windLI.textContent = `Wind: ${weather.wind.speed} mph`
-  weatherInformation.appendChild(windLI)
+  getWeatherByZipCode(zipCode) {
+    let zipURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&APPID=c41f56ca4bf0ad336c93e8389a094c98`
+
+    fetch(zipURL)
+      .then(response => {
+        return response.json()
+      })
+      .then(weatherJson => {
+        let weather = new Weather(weatherJson)
+
+        let dom = new DocumentObjectModel()
+        dom.addWeatherToPage(weather.temp(), weather.humidity(), weather.wind())
+      })
+  }
+}
+
+class Weather {
+  constructor(weather) {
+    this.weather = weather
+  }
+
+  temp() {
+    return this.weather.main.temp
+  }
+
+  humidity() {
+    return this.weather.main.humidity
+  }
+
+  wind() {
+    return this.weather.wind.speed
+  }
+}
+
+class DocumentObjectModel {
+  addWeatherToPage(temp, humidity, wind) {
+    let weatherInformation = document.querySelector('ul')
+    // zap all the html inside that UL (delete whatever might already be there)
+    weatherInformation.innerHTML = ''
+
+    let tempLI = document.createElement('li')
+    tempLI.textContent = `Temperature: ${temp} F`
+    weatherInformation.appendChild(tempLI)
+
+    let humidityLI = document.createElement('li')
+    humidityLI.textContent = `Humidity: ${humidity}%`
+    weatherInformation.appendChild(humidityLI)
+
+    let windLI = document.createElement('li')
+    windLI.textContent = `Wind: ${wind} mph`
+    weatherInformation.appendChild(windLI)
+  }
+
+  getZipCode() {
+    let inputSearch = document.querySelector('.zip-code')
+
+    let zipCode = inputSearch.value
+
+    return zipCode
+  }
+
+  getCityName() {
+    let inputSearch = document.querySelector('.city-name')
+
+    let cityName = inputSearch.value
+
+    return cityName
+  }
 }
 
 const cityWeatherSearch = () => {
-  let inputSearch = document.querySelector('input')
+  let dom = new DocumentObjectModel()
+  let cityName = dom.getCityName()
 
-  let cityName = inputSearch.value
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.getWeatherByCity(cityName)
+}
 
-  let cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&APPID=c41f56ca4bf0ad336c93e8389a094c98`
+const zipWeatherSearch = () => {
+  let dom = new DocumentObjectModel()
+  let zipCode = dom.getZipCode()
 
-  fetch(cityURL)
-    .then(response => {
-      return response.json()
-    })
-    .then(weather => {
-      addWeatherToPage(weather)
-    })
+  let weatherAPI = new WeatherAPI()
+  weatherAPI.getWeatherByZipCode(zipCode)
 }
 
 const main = () => {
-  let searchButton = document.querySelector('.search')
+  let searchButton = document.querySelector('.city')
   searchButton.addEventListener('click', cityWeatherSearch)
+
+  let zipSearchButton = document.querySelector('.zip')
+  zipSearchButton.addEventListener('click', zipWeatherSearch)
 }
 
 document.addEventListener('DOMContentLoaded', main)
